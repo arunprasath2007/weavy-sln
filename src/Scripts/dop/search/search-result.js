2697ca8a253ca1b7511d8509512b60a52151aa3a
Arunprasath <arun_prasath2007@yahoo.co.in>
2018-11-04 08:09:39 +0530

    Moved the script to JS file

---
+++ src/Scripts/dop/search/search-result.js
@@ -0,0 +1,28 @@
+﻿var Dop = Dop || {};
+
+Dop.SearchResults = function () {
+    function hideAll() {
+        $('.search-result-item').hide();
+    }
+    function show(type, event) {
+        hideAll();
+        $('.search-result-item[name=' + type + ']').show();
+        setActiveLink(event);
+        return false;
+    }
+    function showAll(event) {
+        $('.search-result-item').show();
+        setActiveLink(event);
+        return false;
+    }
+    function setActiveLink(event) {
+        $('.nav-link').removeClass('active');
+        $(event.target).addClass('active');
+    }
+
+    return {
+        hideAll,
+        showAll,
+        show
+    }
+}();

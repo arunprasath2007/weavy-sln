3d3754caa6e543e8e51095acdfbaec56001036f7
Arunprasath <arun_prasath2007@yahoo.co.in>
2018-11-14 09:53:18 +0530

    added IFrame app to the weavy sln

---
+++ src/Controllers/IFrameAppController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using Weavy.Core.Models;
+using Weavy.Web.Controllers;
+using Wvy.Models;
+
+namespace Wvy.Controllers
+{
+    [RoutePrefix("apps/{id:int}/98871544-5eaf-4d5b-bc03-ee7af41f8106")]
+    public class IFrameAppController : AppController<IFrameApp>
+    {
+
+        /// <summary>
+        /// Display the specified url in an iframe.
+        /// </summary>
+        /// <param name="app">The app to display.</param>
+        /// <param name="query">An object with query parameters for search, paging etc.</param>
+        public override ActionResult Get(IFrameApp app, Query query)
+        {
+            // add you custom logic here...
+            return View(app);
+        }
+    }
+}
---
+++ src/Models/IFrameApp.cs
@@ -0,0 +1,23 @@
+﻿using Doplace.Dto;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using Weavy.Core.Attributes;
+using Weavy.Core.Models;
+
+namespace Wvy.Models
+{
+    [Serializable]
+    [Guid("98871544-5eaf-4d5b-bc03-ee7af41f8106")]
+    [App(Icon = "application", Name = "IFrame", Description = "Load any third party content here", AllowMultiple = true)]
+    public class IFrameApp : App
+    {
+        [Required]
+        [DataType(DataType.Url)]
+        [Display(Name = "Frame source", Description = "Full url of the resource to embed including http(s)://")]
+        [Uri(ErrorMessage = "Must be a valid and fully-qualified url.")]
+        public string Src { get; set; }
+
+    }
+}

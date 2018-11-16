d966d87d32fc6879e680ed4183637ee7bf8d8c21
Arunprasath <arun_prasath2007@yahoo.co.in>
2018-11-16 15:35:12 +0530

    Changes for the demo app

---
+++ src/Controllers/KanbanAppController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using Weavy.Core.Models;
+using Weavy.Web.Controllers;
+using Wvy.Models;
+
+namespace Wvy.Controllers
+{
+    [RoutePrefix("apps/{id:int}/7e5fb812-71c7-4e1e-be0d-567e06f6dae9")]
+    public class KanbanAppController : AppController<KanbanApp>
+    {
+        /// <summary>
+        /// Display the Task status page.
+        /// </summary>
+        /// <param name="app">The app to display.</param>
+        /// <param name="query">An object with query parameters for search, paging etc.</param>
+        public override ActionResult Get(KanbanApp app, Query query)
+        {
+            return View(app);
+        }
+    }
+}
---
+++ src/Models/KanbanApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using Weavy.Core.Attributes;
+using Weavy.Core.Models;
+
+namespace Wvy.Models
+{
+    [Serializable]
+    [Guid("7e5fb812-71c7-4e1e-be0d-567e06f6dae9")]
+    [App(Icon = "application", Name = "Kanban", Description = "Track your tasks here", AllowMultiple = true)]
+    public class KanbanApp : App
+    {
+    }
+}
